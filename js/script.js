be471bafb0ca8d329ef3fe8593f97743dc850070
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-04-25 14:59:50 +0100

    Shuffle Cards Onclick Refresh Button

--- js/script.js
+++ js/script.js
@@ -9,44 +9,49 @@ playGame.addEventListener("click", function() {
     gameSCreen.classList.remove("hidden");
 });
 
-// Create an array of images from folder
-const imageArray = [
-    "img/1.png",
-    "img/2.png",
-    "img/3.png",
-    "img/4.png",
-    "img/5.png",
-    "img/6.png",
-    "img/7.png",
-    "img/8.png",
-    "img/9.png",
-    "img/10.png",
-    "img/11.png",
-    "img/12.png"
-];
+createImageGrid();
 
-//To select 8 unique random images
-var uniqueImages = [];
-while (uniqueImages.length < 8) {
-    random = imageArray[Math.floor(Math.random() * 12)];
-    if (uniqueImages.indexOf(random) === -1)
-        uniqueImages.push(random);
-}
-// to save the unique images in two random cards
-const gameGrid = uniqueImages
-    .concat(uniqueImages)
-    .sort(() => 0.5 - Math.random());
+function createImageGrid() {
 
-const cardImages = document.querySelectorAll(".card-image");
-gameGrid.forEach((imgStr, index) => {
-    cardImages[index].setAttribute("src", imgStr);
-})
+    // Create an array of images from folder
+    const imageArray = [
+        "img/1.png",
+        "img/2.png",
+        "img/3.png",
+        "img/4.png",
+        "img/5.png",
+        "img/6.png",
+        "img/7.png",
+        "img/8.png",
+        "img/9.png",
+        "img/10.png",
+        "img/11.png",
+        "img/12.png"
+    ];
 
-// flip card and display images
-const cards = document.querySelectorAll(".card");
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", flipCard)
-}
+    //To select 8 unique random images
+    var uniqueImages = [];
+    while (uniqueImages.length < 8) {
+        random = imageArray[Math.floor(Math.random() * 12)];
+        if (uniqueImages.indexOf(random) === -1)
+            uniqueImages.push(random);
+    }
+    // to save the unique images in two random cards
+    const gameGrid = uniqueImages
+        .concat(uniqueImages)
+        .sort(() => 0.5 - Math.random());
+
+    const cardImages = document.querySelectorAll(".card-image");
+    gameGrid.forEach((imgStr, index) => {
+        cardImages[index].setAttribute("src", imgStr);
+    })
+
+    // flip card and display images
+    const cards = document.querySelectorAll(".card");
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", flipCard)
+    }
+};
 
 function flipCard() {
     this.classList.toggle("flip");
@@ -169,6 +174,7 @@ resetGame.addEventListener("click", function() {
         cards[i].classList.remove("flip");
         cards[i].firstElementChild.classList.remove("matched");
     }
+    createImageGrid();
 });
 
 //victory Screen
